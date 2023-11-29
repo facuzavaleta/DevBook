a2c687d2d649463e794d5d99803683648158333f
facuzavaleta <facuzavaleta890@gmail.com>
2023-11-29 18:22:35 -0300

    users model created, pillow installed

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    bio = models.TextField(max_length=500, blank=True)
+    image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
